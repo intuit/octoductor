734c85fd41b149976455f9d9790e63319d284f9b
Frank Wisniewski <coderocker42@gmail.com>
2021-07-01 20:11:55 +0200

    Removing unused classes.

--- lib/index.ts
+++ lib/index.ts
@@ -3,11 +3,9 @@ export * from './common-layer'
 export * from './constructs/health-check'
 
 import * as vpc from './constructs/custom-vpc'
-import * as data from './data/data'
 import * as badge from "./evaluation/badge";
 
 export {
-    data,
     vpc,
     badge
 }
